866b8d286f456f6c1e27297131b8f356004d8cfd
gitlab.com/pepa65 <pepa65@passchier.net>
2021-04-28 22:12:27 +0700

    No arm

--- package.sh
+++ package.sh
@@ -6,28 +6,24 @@ VERSION=$(node -pe "require('./package.json').version")
 rm -rf dist
 mkdir dist
 mkdir releases/dcp-$VERSION-linux-x64
-mkdir releases/dcp-$VERSION-linux-armv7
 mkdir releases/dcp-$VERSION-macos-x64
 mkdir releases/dcp-$VERSION-win-x64
 
+ls releases
 mv releases/dcp-linux releases/dcp-$VERSION-linux-x64/dcp
-mv releases/dcp-linux-armv7 releases/dcp-$VERSION-linux-armv7/dcp
 mv releases/dcp-macos releases/dcp-$VERSION-macos-x64/dcp
 mv releases/dcp-win.exe releases/dcp-$VERSION-win-x64/dcp.exe
 
 cp LICENSE.md releases/dcp-$VERSION-linux-x64/
-cp LICENSE.md releases/dcp-$VERSION-linux-arm/
 cp LICENSE.md releases/dcp-$VERSION-macos-x64/
 cp LICENSE.md releases/dcp-$VERSION-win-x64/
 
 cp README.md releases/dcp-$VERSION-linux-x64/README
-cp README.md releases/dcp-$VERSION-linux-arm/README
 cp README.md releases/dcp-$VERSION-macos-x64/README
 cp README.md releases/dcp-$VERSION-win-x64/README
 
 cd releases
 ../node_modules/.bin/cross-zip dcp-$VERSION-linux-x64 ../dist/dcp-$VERSION-linux-x64.zip
-../node_modules/.bin/cross-zip dcp-$VERSION-linux-arm ../dist/dcp-$VERSION-linux-arm.zip
 ../node_modules/.bin/cross-zip dcp-$VERSION-macos-x64 ../dist/dcp-$VERSION-macos-x64.zip
 ../node_modules/.bin/cross-zip dcp-$VERSION-win-x64 ../dist/dcp-$VERSION-win-x64.zip
 
